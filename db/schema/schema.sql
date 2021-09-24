c8644dd3c62ec293fde97e6d835671d46e58c265
woodpeckershop <chris.sun5567@gmail.com>
2021-09-24 14:53:19 -0700

    reconfig template and add seeds/schema

---
+++ db/schema/schema.sql
@@ -0,0 +1,36 @@
+-- Drop any existing tables to start fresh
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE IF EXISTS services CASCADE;
+DROP TABLE IF EXISTS favorites CASCADE;
+
+-- users table
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone TEXT NOT NULL DEFAULT 0
+);
+-- products table
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  price INTEGER NOT NULL DEFAULT 0,
+  image_path TEXT NOT NULL
+);
+
+-- services table
+CREATE TABLE services (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  price INTEGER NOT NULL DEFAULT 0,
+  image_path TEXT NOT NULL
+);
+-- favorites table
+CREATE TABLE favorites (
+  id SERIAL PRIMARY KEY,
+  product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
