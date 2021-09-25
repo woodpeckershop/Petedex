e51ba0d44f3a2c58b28d42bce23647ba6b8500f8
woodpeckershop <chris.sun5567@gmail.com>
2021-09-25 16:25:12 -0700

    crystal actually worked
    Co-authored-by: smile2682 smilecrystal.liyi@gmail.com

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-
+//this is to show crystal actually worked
 import "./App.css";
 import Checkout from "./Checkout.jsx";
 import Header from "./Header.jsx";
