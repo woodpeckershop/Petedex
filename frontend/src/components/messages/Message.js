cb040f05f634d1843483302a905fff87ed49e609
andysongsong <sxj930916@gmail.com>
2021-10-04 11:14:23 -0700

    finish mfinish message

--- frontend/src/components/messages/Message.js
+++ frontend/src/components/messages/Message.js
@@ -1,20 +1,20 @@
-import Axios from 'axios';
-import React, { useState } from 'react';
+import Axios from "axios";
+import React, { useState } from "react";
 
-function Message({ messages, id, user_id, setMessages }) {
-  const [input, setInput] = useState('');
+function Message({ messages, id, user_id, setMessages, users }) {
+  const [input, setInput] = useState("");
   const handleReply = () => {
     const templateVar = {
       recipient_id: id,
       sender_id: user_id,
       content: input,
     };
-    Axios.put('http://localhost:8080/api/messages', templateVar)
+    Axios.put("http://localhost:8080/api/messages", templateVar)
       .then((data) => {
         setMessages([...messages, data.data[0]]);
       })
       .catch((err) => console.log(err));
-    setInput('');
+    setInput("");
     return;
   };
   const messageList = messages
@@ -22,24 +22,38 @@ function Message({ messages, id, user_id, setMessages }) {
       (message) => message?.recipient_id === id || message?.sender_id === id
     )
     .map((message) => {
-      if (message)
+      if (message && users.length !== 0) {
+        const fromName = users?.filter(
+          (user) => message.sender_id === user.id
+        )[0].name;
+        const toName = users?.filter(
+          (user) => message.recipient_id === user.id
+        )[0].name;
+
         return (
           <tr>
-            <td>From: {message.sender_id}</td>
-            <td>To: {message.recipient_id}</td>
+            <td>From: {fromName}</td>
+            <td>To: {toName}</td>
             <td>Content: {message.content}</td>
           </tr>
         );
+      }
     });
   return (
     <div>
       <table> {messageList}</table>
-      <input
-        type='text'
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button onClick={handleReply}>Reply</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleReply}>Reply</button>
+      </form>
     </div>
   );
 }
--- frontend/src/components/messages/MessageGroup.js
+++ frontend/src/components/messages/MessageGroup.js
@@ -1,12 +1,13 @@
-import Axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react';
-import { authContext } from '../providers/AuthProvider';
-import './MessageGroup.scss';
-import Message from './Message';
+import Axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { authContext } from "../providers/AuthProvider";
+import "./MessageGroup.scss";
+import Message from "./Message";
 
 function MessageGroup() {
   const { user_id } = useContext(authContext);
   const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
 
   const allClientIdList = messages.map((message) => {
     if (message && message.recipient_id !== user_id)
@@ -20,14 +21,24 @@ function MessageGroup() {
     Axios.get(`http://localhost:8080/api/messages/${user_id}`).then((res) => {
       setMessages([...res.data]);
     });
+
+    Axios.get("http://localhost:8080/api/users/all").then((res) => {
+      setUsers([...res.data]);
+    });
   }, []);
 
   const messageGroup = uniqueIdList.map((id) => {
+    let clientName;
+    if (users.length !== 0) {
+      clientName = users.filter((user) => Number(id) === user.id)[0].name;
+    }
+
     return (
       <>
-        <h1>Message with: {id}</h1>
+        <h1>Message with: {clientName}</h1>
         <Message
           messages={messages}
+          users={users}
           id={id}
           user_id={user_id}
           setMessages={setMessages}
