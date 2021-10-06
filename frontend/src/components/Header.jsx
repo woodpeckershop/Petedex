27e014b9d8f3b143801272d7648a18482121b44d
woodpeckershop <chris.sun5567@gmail.com>
2021-10-05 22:12:27 -0700

    change back nav bar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,28 +1,28 @@
-import * as React from "react";
+// import * as React from "react";
 // import { styled, alpha } from "@mui/material/styles";
 // import AppBar from "@mui/material/AppBar";
 // import Box from "@mui/material/Box";
 // import Toolbar from "@mui/material/Toolbar";
 // import Typography from "@mui/material/Typography";
 // import InputBase from "@mui/material/InputBase";
-import { Link } from "@mui/material";
+// import { Link } from "@mui/material";
 
-import { useEffect, useState, useContext } from "react";
-import "./Header.scss";
-import { useHistory } from "react-router-dom";
+// import { useEffect, useState, useContext } from "react";
+// import "./Header.scss";
+// import { useHistory } from "react-router-dom";
 
-import SearchIcon from "@mui/icons-material/Search";
-import MailOutlineIcon from "@mui/icons-material/MailOutline";
+// import SearchIcon from "@mui/icons-material/Search";
+// import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
-import StorefrontIcon from "@mui/icons-material/Storefront";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import LoginIcon from "@mui/icons-material/Login";
-import LogoutIcon from "@mui/icons-material/Logout";
+// import StorefrontIcon from "@mui/icons-material/Storefront";
+// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+// import LoginIcon from "@mui/icons-material/Login";
+// import LogoutIcon from "@mui/icons-material/Logout";
 
-import { logo } from "../assets/images";
-import { IconButton } from "@mui/material";
-import Axios from "axios";
-import { authContext } from "../components/providers/AuthProvider";
+// import { logo } from "../assets/images";
+// import { IconButton } from "@mui/material";
+// import Axios from "axios";
+// import { authContext } from "../components/providers/AuthProvider";
 
 // import { NavLink } from "react-router-dom";
 
@@ -185,18 +185,141 @@ import { authContext } from "../components/providers/AuthProvider";
 //   );
 // }
 
+
+
+
+
+// function Header({ setSelectedItem }) {
+//   const [productName, setProductName] = useState("");
+//   const { user_name, user_id } = useContext(authContext);
+//   const [name, setName] = useState("Guest");
+//   const [status, setStatus] = useState(
+//     <LoginIcon style={{ fill: "#e1533c", fontSize: "400%" }} />
+//   );
+
+//   useEffect(() => {
+//     if (user_name) {
+//       setName(user_name);
+//       setStatus(<LogoutIcon style={{ fill: "#e1533c", fontSize: "400%" }} />);
+//     }
+//   }, [user_name]);
+
+//   // console.log("outside productName", productName);
+//   let history = useHistory();
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     Axios.post("http://localhost:8080/api/products/search", {
+//       productName: productName,
+//     })
+//       .then((res) => {
+//         const searchResult = res.data[0];
+//         setSelectedItem(searchResult);
+//         history.push("/search");
+//       })
+//       .catch((err) => {
+//         console.log("err", err);
+//       });
+//   };
+
+//   const myStoreLink = user_id ? "/Mystore" : "/login";
+//   const myMessagesLink = user_id ? "/mymessages" : "/login";
+//   const myFavLink = user_id ? "/favorites" : "/login";
+
+//   return (
+//     <div className="header">
+//       {
+//         <form className="header_searchForm" onSubmit={handleSubmit}>
+//           <input
+//             className="header_searchInput"
+//             type="text"
+//             placeholder="Please type product here."
+//             value={productName}
+//             onChange={(e) => setProductName(e.target.value)}
+//             size="50"
+//           />
+//           <IconButton type="submit">
+//             <SearchIcon
+//               style={{ fill: "#e1533c", fontSize: "400%" }}
+//               className="header_searchIcon"
+//             />
+//           </IconButton>
+//         </form>
+//       }
+
+//       {
+//         <Link to="/">
+//           <img alt="logo" className="header_logo" src={logo} />
+//         </Link>
+//       }
+
+//       <div className="header-right">
+//         <Link to="/login">
+//           <div className="header_login">
+           
+//             <span className="header-right-font">{`Hello ${name}`}</span>
+//             <span>{status}</span>
+//           </div>
+//         </Link>
+//         <Link to={myStoreLink}>
+//           <StorefrontIcon
+//             className="header-right-button"
+//             style={{ 
+//               // fill: "#e1533c", 
+//               fontSize: "400%" }}
+//           />
+//         </Link>
+
+//         <Link to={myMessagesLink}>
+//           <MailOutlineIcon
+//             className="header-right-button"
+//             style={{ 
+//               // fill: "#e1533c", 
+//               fontSize: "400%" }}
+//           />
+//         </Link>
+
+//         <Link to={myFavLink}>
+//           <FavoriteBorderIcon
+//             className="header-right-button"
+//             style={{ 
+//               // fill: "#e1533c", 
+//               fontSize: "400%" }}
+//           />
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Header;
+
+import { useEffect, useState, useContext } from "react";
+import "./Header.scss";
+import { Link, useHistory } from "react-router-dom";
+
+import SearchIcon from "@mui/icons-material/Search";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+import { logo } from "../assets/images";
+import { IconButton } from "@mui/material";
+import Axios from "axios";
+import { authContext } from "../components/providers/AuthProvider";
+
 function Header({ setSelectedItem }) {
   const [productName, setProductName] = useState("");
   const { user_name, user_id } = useContext(authContext);
   const [name, setName] = useState("Guest");
-  const [status, setStatus] = useState(
-    <LoginIcon style={{ fill: "#e1533c", fontSize: "400%" }} />
-  );
+  const [status, setStatus] = useState(<LoginIcon style={{ fill: "white", fontSize: "400%" }}/>);
 
   useEffect(() => {
     if (user_name) {
       setName(user_name);
-      setStatus(<LogoutIcon style={{ fill: "#e1533c", fontSize: "400%" }} />);
+      setStatus(<LogoutIcon style={{ fill: "white", fontSize: "400%" }}/>);
     }
   }, [user_name]);
 
@@ -223,7 +346,8 @@ function Header({ setSelectedItem }) {
 
   return (
     <div className="header">
-      {
+     
+      
         <form className="header_searchForm" onSubmit={handleSubmit}>
           <input
             className="header_searchInput"
@@ -231,56 +355,36 @@ function Header({ setSelectedItem }) {
             placeholder="Please type product here."
             value={productName}
             onChange={(e) => setProductName(e.target.value)}
-            size="50"
           />
+
           <IconButton type="submit">
-            <SearchIcon
-              style={{ fill: "#e1533c", fontSize: "400%" }}
-              className="header_searchIcon"
-            />
+            <SearchIcon className="header_searchIcon" />
           </IconButton>
         </form>
-      }
+      
 
-      {
-        <Link to="/">
-          <img alt="logo" className="header_logo" src={logo} />
-        </Link>
-      }
+      <Link to="/">
+        <img alt="logo" className="header_logo" src={logo} />
+      </Link>
 
       <div className="header-right">
         <Link to="/login">
           <div className="header_login">
-           
-            <span className="header-right-font">{`Hello ${name}`}</span>
-            <span>{status}</span>
+            <span >{`Hello ${name}`}</span>
+            <span >{status}</span>
           </div>
         </Link>
+
         <Link to={myStoreLink}>
-          <StorefrontIcon
-            className="header-right-button"
-            style={{ 
-              // fill: "#e1533c", 
-              fontSize: "400%" }}
-          />
+          <StorefrontIcon style={{ fill: "white", fontSize: "400%" }} />
         </Link>
 
         <Link to={myMessagesLink}>
-          <MailOutlineIcon
-            className="header-right-button"
-            style={{ 
-              // fill: "#e1533c", 
-              fontSize: "400%" }}
-          />
+          <MailOutlineIcon style={{ fill: "white", fontSize: "400%" }} />
         </Link>
 
         <Link to={myFavLink}>
-          <FavoriteBorderIcon
-            className="header-right-button"
-            style={{ 
-              // fill: "#e1533c", 
-              fontSize: "400%" }}
-          />
+          <FavoriteBorderIcon style={{ fill: "white", fontSize: "400%" }} />
         </Link>
       </div>
     </div>
